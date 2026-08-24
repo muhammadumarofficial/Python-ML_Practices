a03254b4b67fef9226243c1af37e86346ec95cb4
Muhammad Umar <syedmuhammadumarofficial@gmail.com>
2026-08-24 19:27:13 +0500

    last updated Lab2

--- ml_1st_lab_practice.py
+++ ml_1st_lab_practice.py
@@ -1,108 +1,133 @@
-#git add .
-#git commit -m "msg"
-#git push
+# #git add .
+# #git commit -m "msg"
+# #git push
 
 
-print("hello wordl")
+# print("hello wordl")
 
-#varibales 
-name="umar"
-age=20
-mark=99
-gpa = float(3.52)
+# #varibales 
+# name="umar"
+# age=20
+# mark=99
+# gpa = float(3.52)
 
-print(name)
-print(age)
-print(mark)
-print(gpa)
+# print(name)
+# print(age)
+# print(mark)
+# print(gpa)
 
-# Arithmetic operator
-add = 5+5
-sub = 50-30
-mul = 5*8
-div = 100/2
-floor_div = 15//2
-power = 2**3
-remainder = 10%3
+# # Arithmetic operator
+# add = 5+5
+# sub = 50-30
+# mul = 5*8
+# div = 100/2
+# floor_div = 15//2
+# power = 2**3
+# remainder = 10%3
 
-print("add :", add)
-print("sub :", sub)
-print("mul :", mul)
-print("div:", div)
-print(floor_div)
-print(power)
-print(remainder)
+# print("add :", add)
+# print("sub :", sub)
+# print("mul :", mul)
+# print("div:", div)
+# print(floor_div)
+# print(power)
+# print(remainder)
 
-#Comparison operators
-a = 10
-b = 20
+# #Comparison operators
+# a = 10
+# b = 20
 
-print(a == b)
-print(a != b)
-print(a > b)
-print(a < b)
-print(a >= b)
-print(a <= b)
+# print(a == b)
+# print(a != b)
+# print(a > b)
+# print(a < b)
+# print(a >= b)
+# print(a <= b)
 
-#Logical Operators 
-print("logical operators")
-salary = 1000
+# #Logical Operators 
+# print("logical operators")
+# salary = 1000
 
-print(salary>=1000 and salary<=2000 )
-print(salary<1000 or salary>2000)
+# print(salary>=1000 and salary<=2000 )
+# print(salary<1000 or salary>2000)
 
-x=True
-print(not x)
+# x=True
+# print(not x)
 
-#Conditional Statements
+# #Conditional Statements
 
-x = 50
-if x<=50:
-    print("number is less and equal")
-else:
-    print("number is greater")
+# x = 50
+# if x<=50:
+#     print("number is less and equal")
+# else:
+#     print("number is greater")
 
-# if else with input
-age = int(input("enter your age "))
+# # if else with input
+# age = int(input("enter your age "))
 
-if age>=18 and age<=50:
-    print("you are adult ")
-elif age>50:
-    print("you are old")
-else:
-    print("your are child")
+# if age>=18 and age<=50:
+#     print("you are adult ")
+# elif age>50:
+#     print("you are old")
+# else:
+#     print("your are child")
 
-#nested if
-has_id = True
+# #nested if
+# has_id = True
 
-if age>=18:
-    if has_id:
-        print("allow to vist")
-    else:
-        print("not allowed")
-else:
-    print("you are not eligible")
+# if age>=18:
+#     if has_id:
+#         print("allow to vist")
+#     else:
+#         print("not allowed")
+# else:
+#     print("you are not eligible")
 
-#number indentifyer
-num = int(input("Enter a number: "))
+# #number indentifyer
+# num = int(input("Enter a number: "))
 
-if num > 0:
-    print("Positive")
-elif num < 0:
-    print("Negative")
-else:
-    print("Zero")
+# if num > 0:
+#     print("Positive")
+# elif num < 0:
+#     print("Negative")
+# else:
+#     print("Zero")
 
-#loops
-#for i in range(3):
- #   print(i)
+# #loops
+# for i in range(3):
+#    print(i)
 
-# range types maans controlling the stop and start 
-for i in range(2,10):
-    print(i)
+# # range types maans controlling the stop and start 
+# for i in range(2,10):
+#     print(i)
 
-for i in range(5,40,5):
-    print(i)
+# #range(start, stop, step)
+# for i in range(5,40,5):
+#     print(i)
+
+# # WHILE LOOP
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i += 1
+
+#user define function
+def addTwoNum(*args):
+    return sum(10,10)
+
+print(addTwoNum)
+
+
+
+
+
+
+
+
+
+
+
+
     
 
 
